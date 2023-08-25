1adeed3f55445d6f6f50612e93e434274206d8f5
Joao Victor Da Silva Guedes <760fd_joao@callink.com.br>
2023-08-25 17:12:53 -0300

    Tetando implementar ideias...

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const botoesCaes = document.querySelectorAll('.cabecalho__botoes_caes');
-const botoesGatos = document.querySelectorAll('.cabecalho__botoes_gatos');
-const racasCaes = document.querySelectorAll('.menu__caes');
-const racasGatos = document.querySelectorAll('.menu__gatos');
+// const botoesCaes = document.querySelectorAll('.cabecalho__botoes_caes');
+// const botoesGatos = document.querySelectorAll('.cabecalho__botoes_gatos');
+// const racasCaes = document.querySelectorAll('.menu__caes');
+// const racasGatos = document.querySelectorAll('.menu__gatos');
 
-botoesCaes.forEach((link, index) => {
-    link.addEventListener('mouseenter', () =>{
-        racasCaes[index].style.display = 'block';
-    });
+// botoesCaes.forEach((link, index) => {
+//     link.addEventListener('mouseenter', () =>{
+//         racasCaes[index].style.display = 'block';
+//     });
 
-    link.addEventListener('mouseleave', () => {
-        racasCaes[index].style.display = 'none';
-    });
-});
+//     link.addEventListener('mouseleave', () => {
+//         racasCaes[index].style.display = 'none';
+//     });
+// });
 
-botoesGatos.forEach((link, index) => {
-    link.addEventListener('mouseenter', () =>{
-        racasGatos[index].style.display = 'block';
-    });
+// botoesGatos.forEach((link, index) => {
+//     link.addEventListener('mouseenter', () =>{
+//         racasGatos[index].style.display = 'block';
+//     });
 
-    link.addEventListener('mouseleave', () => {
-        racasGatos[index].style.display = 'none';
-    });
-});
+//     link.addEventListener('mouseleave', () => {
+//         racasGatos[index].style.display = 'none';
+//     });
+// });
