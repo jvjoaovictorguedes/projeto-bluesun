f5abde4bac4595646b14e7b43043663c7fbf4c8d
Joao Victor Da Silva Guedes <760fd_joao@callink.com.br>
2023-09-20 16:30:18 -0300

    Alteração de tudo!

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// const botoesCaes = document.querySelectorAll('.cabecalho__botoes_caes');
-// const botoesGatos = document.querySelectorAll('.cabecalho__botoes_gatos');
-// const racasCaes = document.querySelectorAll('.menu__caes');
-// const racasGatos = document.querySelectorAll('.menu__gatos');
-
-// botoesCaes.forEach((link, index) => {
-//     link.addEventListener('mouseenter', () =>{
-//         racasCaes[index].style.display = 'block';
-//     });
-
-//     link.addEventListener('mouseleave', () => {
-//         racasCaes[index].style.display = 'none';
-//     });
-// });
-
-// botoesGatos.forEach((link, index) => {
-//     link.addEventListener('mouseenter', () =>{
-//         racasGatos[index].style.display = 'block';
-//     });
-
-//     link.addEventListener('mouseleave', () => {
-//         racasGatos[index].style.display = 'none';
-//     });
-// });
+document.getElementById('menuHamburguer').addEventListener('mouseover', function() {
+    document.querySelector('.cabecalho__menu').classList.toggle('active');
+});
